a0027738bb01e2c7ddcf1438a96c67590cefb9a8
Vladimir horev <vshorev@yandex.ru>
2024-03-04 22:34:02 +0700

    first commmit

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -10,28 +10,31 @@ function App() {
     return (
 
 
+        <>
+            <nav class="  bg-dark ">
 
-        <nav class="  bg-dark ">
+                <ul class=" d-flex justify-content-around">
+                    <li class="nav-item m-2 px-4 "> <NavLink className='link-light' to={'/killing'}
+                    > Глушение </NavLink>
+                    </li>
 
-            <ul class=" d-flex justify-content-around">
-                <li class="nav-item m-2 px-4 "> <NavLink className='link-light' to={'/killing'}
-                > Глушение </NavLink>
-                </li>
+                    <li class="nav-item m-2 px-4 "> <NavLink className='link-light' to={'/drillmud'}
+                    >Буровые растворы </NavLink>
+                    </li>
 
-                <li class="nav-item m-2 px-4 "> <NavLink className='link-light' to={'/drillmud'}
-                >Буровые растворы </NavLink>
-                </li>
+                    <li class="nav-item m-2  px-4"> <NavLink className='link-light' to={'/nnb'}
+                    >ННБ </NavLink>
+                    </li>
 
-                <li class="nav-item m-2  px-4"> <NavLink className='link-light' to={'/nnb'}
-                >ННБ </NavLink>
-                </li>
+                    <li class="nav-item m-2  px-4"> <NavLink className='link-light' to={'/other_calculations'}
+                    >Прочие расчеты </NavLink>
+                    </li>
+                </ul>
 
-                <li class="nav-item m-2  px-4"> <NavLink className='link-light' to={'/other_calculations'}
-                >Прочие расчеты </NavLink>
-                </li>
-            </ul>
+            </nav>
 
-        </nav>
+            <div className="text-center  more fs-1 fw-bold  text-light" > Bohren macht frei  </div>
+        </>
     );
 }
 
